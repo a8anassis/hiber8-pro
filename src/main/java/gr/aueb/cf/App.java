0dd4fbd259370443327819334aad790f9a11988e
a8anassis <a8anassis@gmail.com>
2025-07-02 10:23:04 +0300

    feat: add crud

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -1,8 +1,18 @@
 package gr.aueb.cf;
 
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class App {
+
+    private final static EntityManagerFactory emf = Persistence.createEntityManagerFactory("school8PU");
+    private final static EntityManager em = emf.createEntityManager();
+
     public static void main(String[] args) {
+
+        // CRUD
 
     }
 }
