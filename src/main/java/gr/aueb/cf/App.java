486f698b716a1d7ecfdaec722bec25d9164de33a
a8anassis <a8anassis@gmail.com>
2025-07-02 20:01:03 +0300

    feat: add queries logic with JPQL

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -1,9 +1,14 @@
 package gr.aueb.cf;
 
 
+import gr.aueb.cf.model.Course;
+import gr.aueb.cf.model.Teacher;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
 
 public class App {
 
@@ -12,8 +17,48 @@ public class App {
 
     public static void main(String[] args) {
 
+//        Teacher teacher = new Teacher(null, "Αθανάσιος", "Ανδρούτσος");
+//        Course course = new Course(null, "Java");
+//        teacher.addCourse(course);
 
 
+        // JPQL (Java Persistence Query Language)
+        // Criteria - API
 
+        em.getTransaction().begin();
+
+//        Course course = em.find(Course.class, 1L);
+//        em.remove(course);
+
+//        Teacher teacher = em.find(Teacher.class, 1L);
+//        em.remove(teacher);
+//        teacher.setFirstname("Αθ.");
+//        em.merge(teacher);
+
+//        em.persist(teacher);
+//        em.persist(course);
+
+
+        // Select all teachers
+        String sql = "SELECT t FROM Teacher t";
+        List<Teacher> teachers = em.createQuery(sql, Teacher.class).getResultList();
+        teachers.forEach(System.out::println);
+
+
+        //SELECT courses του teacher με επώνυμο 'Ανδρούτσος'
+        //String sql2 = "SELECT c FROM Course c WHERE c.teacher.lastname = 'Ανδρούτσος'";
+        String sql2 = "SELECT c FROM Course c WHERE c.teacher.lastname = :lastname";
+        TypedQuery<Course> query = em.createQuery(sql2, Course.class);
+        List<Course> courses = query.setParameter("lastname", "Ανδρούτσος")
+                        .getResultList();
+        courses.forEach(System.out::println);
+
+
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
     }
 }
+
+
